6e2d28990e1839b41d0159c64b339e28ba5f1e2a
Steven Pawley <dr.stevenpawley@gmail.com>
2022-12-20 08:56:39 -0700

    add airdata.rds

---
+++ scripts/update-batch.R
@@ -0,0 +1,20 @@
+library(here)
+library(airdataAB)
+library(dplyr)
+library(lubridate)
+library(purrr)
+
+airdata <- readRDS(here("data/airdata.rds"))
+
+last_dates <- 
+  left_join(airdata, parameters_aqhi, by = c("station", "measure")) |> 
+  filter(type == "Operational") |> 
+  group_by(station) |> 
+  summarize(last_date = max(datetime, na.rm = TRUE)) |> 
+  filter(year(last_date) >= 2022)
+
+new_data <- 
+  map2_dfr(last_dates$station, last_dates$last_date, function(sta, dttm) {
+    collect_air(sta, start = dttm, end = now(),  source = "aqhi")
+  })
+
